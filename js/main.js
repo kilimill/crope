d019b310ccc25dc44787b36e73ab9a08246b904f
kilimill <kilimill2585@gmail.com>
2020-09-20 22:36:31 +0700

    start

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(function(){
+
+  // var typed = new Typed('.element', {
+  //   // 
+  //   typeSpeed: 300,
+  //   backSpeed: 200,
+  //   loop: true,
+  //   loopCount: Infinity,
+  //   stringElement: $('#title__text')
+  // });
+
+
+
+  var typed = new Typed('.element-1', {
+
+    typeSpeed: 100,
+    startDelay: 0,
+    stringsElement: '#tre-1'
+  });
+
+  var typed = new Typed('.element-2', {
+
+    typeSpeed: 100,
+    startDelay: 1200,
+    stringsElement: '#tre-2'
+  });
+
+  var typed = new Typed('.element-3', {
+
+    typeSpeed: 100,
+    startDelay: 2600,
+    stringsElement: '#tre-3'
+  });
+
+  var typed = new Typed('.element-4', {
+
+    typeSpeed: 100,
+    startDelay: 3500,
+    stringsElement: '#tre-4'
+  });
+
+  var typed = new Typed('.element-5', {
+
+    typeSpeed: 100,
+    startDelay: 4200,
+    backSpeed: 100,
+    stringsElement: '#tre-5',
+    loop: true,
+    loopCount: Infinity,
+  });
+
+});
