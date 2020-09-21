99a8203815098f1900689245076ec095613f07ee
kilimill <kilimill2585@gmail.com>
2020-09-21 22:43:43 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -12,41 +12,46 @@ $(function(){
 
 
   var typed = new Typed('.element-1', {
-
+    showCursor: false,
     typeSpeed: 100,
     startDelay: 0,
     stringsElement: '#tre-1'
   });
 
   var typed = new Typed('.element-2', {
-
+    showCursor: false,
     typeSpeed: 100,
-    startDelay: 1200,
+    // startDelay: 1200,
     stringsElement: '#tre-2'
   });
 
   var typed = new Typed('.element-3', {
-
-    typeSpeed: 100,
-    startDelay: 2600,
+    showCursor: false,
+    typeSpeed: 200,
+    // startDelay: 2600,
     stringsElement: '#tre-3'
   });
 
   var typed = new Typed('.element-4', {
-
-    typeSpeed: 100,
-    startDelay: 3500,
+    showCursor: false,
+    typeSpeed: 300,
+    // startDelay: 3500,
     stringsElement: '#tre-4'
   });
 
   var typed = new Typed('.element-5', {
 
-    typeSpeed: 100,
-    startDelay: 4200,
+    typeSpeed: 300,
+    // startDelay: 4200,,
+    showCursor: false,
     backSpeed: 100,
     stringsElement: '#tre-5',
     loop: true,
     loopCount: Infinity,
+  });
+
+  $('.burger__menu').magnificPopup({
+    type: 'inline'
   });
 
 });
